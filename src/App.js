79421acd51d06a45b25c4cf61a10cda3e07a16ae
JamesLeggero <james.m.leggero@gmail.com>
2020-09-06 15:11:43 -0400

    dropdown search working for two categories

--- src/App.js
+++ src/App.js
@@ -25,45 +25,19 @@ function App(props) {
   })
 
 
-  // let num1= 1
-  // /
-  // async function fetchData(){
-  // const fetchData= async() =>{
-  //   try{
-
-
-  //   const response= await axios.get(
-  //       `https://api.harvardartmuseums.org/object?apikey=bogusapikey${searchQuery}`
-  //     )
-  //     setAllReturnedObjects(response.data);
-  //     }catch(error){
-  //       console.error(error)
-  //     }
-  //     fetchData()
-  //     num1=0
-  //     console.log(allReturnedObjects)
-  // },[allReturnedObjects])
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     query.searchURL.length > 0 &&
-        
-  //         (const response = await axios.get(query.searchURL)
-  //         setAllReturnedObjects(response.data)
-  //         updateQuery({...query, searchQuery: '', categories: ''}    
-  //   })
-     
-  //     fetchData(
-  // })
-
   useEffect(() => {
 		query.searchURL.length > 0 &&
 			(async () => {
 				try {
-					const response = await fetch(query.searchURL);
-					const data = await response.json(); // this is the point where we now have the movie object, NOT the response object itself
-					setAllReturnedObjects({ ...allReturnedObjects, ...data });
-					// console.log(allReturnedObjects);
+
+					// const response = await fetch(query.searchURL);
+          // const data = await response.json(); 
+          // setAllReturnedObjects({ ...allReturnedObjects, ...data });
+          console.log(query.searchURL)
+          const response = await axios.get(query.searchURL)
+          setAllReturnedObjects({ ...allReturnedObjects, ...response.data });
+          
+
 					updateQuery({ ...query, searchURL: '', categories: '' });
 				} catch (error) {
 					console.error(error);
@@ -72,11 +46,38 @@ function App(props) {
   }, [allReturnedObjects, query]);
   
   const handleSubmit = event => {
-		event.preventDefault();
+    event.preventDefault();
+    console.log(typeof document.getElementById('cultureSelect').value)
+    const returnedCultureValue = 
+      document.getElementById('cultureSelect').value === '' ?
+      'culture=any&' : 
+    'culture=' + document.getElementById('cultureSelect').value + '&';
+
+    // const returnedCultureValue = 
+    //   (document.getElementById('cultureSelect').value) ?
+    //   'hello' : 
+    // 'culture=' + document.getElementById('cultureSelect').value + '&';
+    const returnedClassificationValue = 'classification=' + document.getElementById('classificationSelect').value
+    // const returnedCultureValue = 'culture=' + document.getElementById('cultureSelect').value + '&';
+    console.log(returnedCultureValue)
+    // query.categories= 'culture=' + document.getElementById('cultureSelect').value
+    // query.categories= 'technique=' + document.getElementById('techniqueSelect').value
+    // console.log(returnedCultureValue)
+    updateQuery({
+      ...query, ...query.categories = returnedCultureValue + returnedClassificationValue
+    })
+    query.categories.length > 0 ?
 		updateQuery({
 			...query,
-			searchURL: `${query.baseURL}${query.apiKey}&${query.categories}`
-		});
+      searchURL: `${query.baseURL}q=${query.categories}&totalrecordsperquery=30&${query.apiKey}`}) : 
+    
+      updateQuery({
+        ...query,
+        searchURL: `${query.baseURL}${query.apiKey}`
+
+    });
+  
+    
   };
   
   const handleChange = event => {
@@ -105,7 +106,27 @@ function App(props) {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-      <label htmlFor="title">
+
+      <label htmlFor='culture-select'>Select Culture</label>
+        <select id='cultureSelect' name='culture' value={undefined} onChange={handleChange}>
+          <option value=''>Select Culture...</option>
+          <option value='French'>French</option>
+          <option value='Greek'>Greek</option>
+          <option value='Italian'>Italian</option>
+          <option value='Chinese'>Chinese</option>
+          
+        </select>
+
+        <label htmlFor='classification-select'>Select Classification</label>
+        <select id='classificationSelect' name='classification' value={undefined} onChange={handleChange}>
+          <option value=''>Select Classification...</option>
+          <option value='Photographs'>Photographs</option>
+          <option value='Vessels'>Vessels</option>
+          <option value='Coins'>Coins</option>
+          
+        </select>
+        
+      {/* <label htmlFor="title"> // ORIGINAL TYPE-IN STYLE
 					Title:
 					<input
 						id="categories"
@@ -113,19 +134,20 @@ function App(props) {
 						value={query.categories}
 						onChange={handleChange}
 					/>
-				</label>
+				</label> */}
+
 				<input type="submit" value="Search For Objects" />
         
       </form>
+      {/* {console.log(allReturnedObjects.info)} */}
       {Object.keys(allReturnedObjects).length > 0 &&
       // {showReturnedObjects}
       allReturnedObjects.records.map((object,i)=>{
         return(
           <div key={i}> 
-          <img src={object.primaryimageurl} alt='art piece' style={{maxWidth: '300px'}}></img>
+          <img src={object.primaryimageurl} alt='art piece' style={{maxWidth: '100px'}}></img>
           <h2>{object.title}</h2>
-          <h2>{object.division}</h2>
-          <h1>{i}</h1>
+
           </div>
         )})
 
