49789e6072590e28e985042e84442e7944d56f08
Alex Yanovich <alexyanovich@Alexs-MacBook-Pro.local>
2020-09-08 13:04:31 -0400

    my collection index working

--- src/components/MyCollection.js
+++ src/components/MyCollection.js
@@ -1,21 +1,35 @@
 import React from 'react'
+import Show from "./Show.js"
+import axios from "axios"
+import App from "../App"
+import {Link} from "react-router-dom"
+
 
 export default function MyCollection(props){
 
     // const [ myObjects ] = this.props
     return(
-        <div className='my-collection'>
+
+      <div>
+        <div className='collection-index'>
         {props.myObjects.map((myObject, i)=>{
           return (
             <div key={myObject._id}>
               <h2>{myObject.title}</h2>
               <img src={myObject.img} style={{maxWidth: '75px'}} alt={myObject.title}/>
               <h2>{myObject.culture}</h2>
+              <Link to={myObject._id}>
+                {/* <Show myObject={myObject} /> */}
+              </Link>
           <p style={{fontSize: '15px'}}>{myObject.description}</p>
             </div>  
           )
+          
         })}
 
       </div>
+      {/* <Show myObject={myObject}/> */}
+        </div>
+      
     )
 }
---
+++ src/components/Show.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+export default function Show (props){
+     return(
+          <div>
+               {/* <img src={props.myObject}/> */}
+               <h1>test</h1>
+          </div>
+     )
+} 
