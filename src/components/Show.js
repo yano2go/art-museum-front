0691bb5d3387dcfb68358b54d6f6cbddfa672385
JamesLeggero <james.m.leggero@gmail.com>
2020-09-10 19:39:52 -0400

    added artist and dated on BE as well

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 
 export default function Show(props){
@@ -7,12 +8,15 @@ export default function Show(props){
 
      return(
           <div>
+               {Object.keys(props.myShownPiece).length > 0 &&
+               <div>
                <h1>{props.myShownPiece.title}</h1>
-               {/* {
-               props.myShownPiece.img ?
-               <img style={{maxWidth: '200px'}} src={props.myShownPiece.img} alt={props.myShownPiece.title}/> :
-               <img style={{maxWidth: '200px'}} src={props.ComingSoon} alt={props.myShownPiece.title}/>
-          } */}
+               <h3>{`(${props.myShownPiece.artist}, ${props.myShownPiece.dated})`}</h3>
+               </div>
+               }
+              
+
+
                <img style={{maxWidth: '200px'}} src={
                     (!props.myShownPiece.img || props.myShownPiece.img === undefined) && Object.keys(props.myShownPiece).length > 0 ? 
                     props.ComingSoon :
